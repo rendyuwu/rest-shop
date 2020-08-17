e1fd618fe2db889b2a44a46d5419f9ee21227cc1
rendyuwu <rendy.case@gmail.com>
2020-08-17 20:52:18 +0700

    menambahkan logging dan error handling

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRouter = require('./api/routes/products');
 const orderRouter = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+// Routes wich should handle request
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+        .json({
+            error: {
+                message: error.message
+            }
+        });
+});
 
 module.exports = app;
